ce00328c898e5132649034950f3c2a4936929933
syalot <syalot@myapplewallet.com>
2019-03-15 11:14:39 +0900

    add: webpack-dev-server 적용

---
+++ webpack.config.dev.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    publicPath: '/',
+    filename: '[name].js',
+    chunkFilename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }, {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  devServer: {
+    index: 'index.html',
+    contentBase: path.join(__dirname, 'public'),
+    hot: true,
+    inline: true,
+    open: true,
+    port: 3000,
+    historyApiFallback: true
+  },
+  performance: {
+    hints: false
+  }
+};
